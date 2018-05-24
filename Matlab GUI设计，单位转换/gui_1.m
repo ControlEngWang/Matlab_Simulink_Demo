d9f8cb7878b5a42ab9ba2df6815abe60e2f88adb
ControlEngWang <1626669708@qq.com>
2018-05-24 21:55:19 +0800

    添加 Matlab GUI设计，单位转换

---
+++ Matlab GUI设计，单位转换/gui_1.m
@@ -0,0 +1,207 @@
+function varargout = gui_1(varargin)
+% GUI_1 MATLAB code for gui_1.fig
+%      GUI_1, by itself, creates a new GUI_1 or raises the existing
+%      singleton*.
+%
+%      H = GUI_1 returns the handle to a new GUI_1 or the handle to
+%      the existing singleton*.
+%
+%      GUI_1('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in GUI_1.M with the given input arguments.
+%
+%      GUI_1('Property','Value',...) creates a new GUI_1 or raises
+%      the existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before gui_1_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to gui_1_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help gui_1
+
+% Last Modified by GUIDE v2.5 24-May-2018 21:12:12
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @gui_1_OpeningFcn, ...
+                   'gui_OutputFcn',  @gui_1_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+% --- Executes just before gui_1 is made visible.
+function gui_1_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to gui_1 (see VARARGIN)
+
+% Choose default command line output for gui_1
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+initialize_gui(hObject, handles, false);
+
+% UIWAIT makes gui_1 wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = gui_1_OutputFcn(hObject, eventdata, handles)
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes during object creation, after setting all properties.
+function density_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to density (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function density_Callback(hObject, eventdata, handles)
+% hObject    handle to density (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of density as text
+%        str2double(get(hObject,'String')) returns contents of density as a double
+density = str2double(get(hObject, 'String'));
+if isnan(density)
+    set(hObject, 'String', 0);
+    errordlg('Input must be a number','Error');
+end
+
+% Save the new density value
+handles.metricdata.density = density;
+guidata(hObject,handles)
+
+% --- Executes during object creation, after setting all properties.
+function volume_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to volume (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function volume_Callback(hObject, eventdata, handles)
+% hObject    handle to volume (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of volume as text
+%        str2double(get(hObject,'String')) returns contents of volume as a double
+volume = str2double(get(hObject, 'String'));
+if isnan(volume)
+    set(hObject, 'String', 0);
+    errordlg('Input must be a number','Error');
+end
+
+% Save the new volume value
+handles.metricdata.volume = volume;
+guidata(hObject,handles)
+
+% --- Executes on button press in calculate.
+function calculate_Callback(hObject, eventdata, handles)
+% hObject    handle to calculate (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+mass = handles.metricdata.density * handles.metricdata.volume;
+set(handles.mass, 'String', mass);
+
+% --- Executes on button press in reset.
+function reset_Callback(hObject, eventdata, handles)
+% hObject    handle to reset (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+initialize_gui(gcbf, handles, true);
+
+% --- Executes when selected object changed in unitgroup.
+function unitgroup_SelectionChangedFcn(hObject, eventdata, handles)
+% hObject    handle to the selected object in unitgroup 
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% handles.metricdata.density
+% handles.metricdata.volume
+
+if (hObject == handles.english)
+    set(handles.text4, 'String', 'lb/cu.in');
+    set(handles.text5, 'String', 'cu.in');
+    set(handles.text6, 'String', 'lb');
+    
+    handles.metricdata.density = handles.metricdata.density/61023.84*2.20;
+    handles.metricdata.volume = handles.metricdata.volume*61023.84;
+else
+    set(handles.text4, 'String', 'kg/cu.m');
+    set(handles.text5, 'String', 'cu.m');
+    set(handles.text6, 'String', 'kg');
+    
+    handles.metricdata.density = handles.metricdata.density*61023.84/2.20;
+    handles.metricdata.volume = handles.metricdata.volume/61023.84;
+end
+set(handles.density, 'String', handles.metricdata.density);
+set(handles.volume,  'String', handles.metricdata.volume);
+guidata(hObject,handles);
+
+
+% --------------------------------------------------------------------
+function initialize_gui(fig_handle, handles, isreset)
+% If the metricdata field is present and the reset flag is false, it means
+% we are we are just re-initializing a GUI by calling it from the cmd line
+% while it is up. So, bail out as we dont want to reset the data.
+if isfield(handles, 'metricdata') && ~isreset
+    return;
+end
+
+handles.metricdata.density = 2;
+handles.metricdata.volume  = 1;
+
+set(handles.density, 'String', handles.metricdata.density);
+set(handles.volume,  'String', handles.metricdata.volume);
+set(handles.mass, 'String', 0);
+
+set(handles.unitgroup, 'SelectedObject', handles.english);
+
+set(handles.text4, 'String', 'lb/cu.in');
+set(handles.text5, 'String', 'cu.in');
+set(handles.text6, 'String', 'lb');
+
+% Update handles structure
+guidata(handles.figure1, handles);
